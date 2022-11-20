b24fdb07848d97d95f147339558acffe9d3085b4
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-11-20 16:59:41 +0000

    fix: throw child process errors

--- src/util/ffmpeg.js
+++ src/util/ffmpeg.js
@@ -5,11 +5,12 @@ import ffmpegPath from 'ffmpeg-static'
 export default (args, { silent = false, pipe } = {}) => new Promise((resolve, reject) => {
     const child = spawn(ffmpegPath, args)
     let err = ''
-    
+
     child.stdout.on('data', (data) => silent || console.log(data.toString()))
-    child.stderr.on('data', (data) => silent || (console.log(data.toString()), err += data.toString()))
-    
+    child.stderr.on('data', (data) => (err += data, silent || console.log(data.toString())))
+
     if (pipe) child.stderr.pipe(pipe)
-    
+
+    child.on('error', reject)
     child.on('exit', (code) => code ? reject(err) : resolve())
 })
