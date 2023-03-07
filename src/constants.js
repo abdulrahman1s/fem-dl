97e856067f635b5195c11a39987336822a59c229
Jose Santos <josemsantos1994@gmail.com>
2023-03-07 11:33:27 -0800

    1080p fix
    Attempt to fix issue #14

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const CAPTION_EXT = 'vtt'
 export const QUALITY_FORMAT = {
     2160: 'index_2160p_Q10_20mbps',
     1440: 'index_1440p_Q10_9mbps',
-    1080: 'index_1080_Q10_7mbps',
+    1080: 'index_1080_Q8_7mbps',
     720: 'index_720_Q8_5mbps',
     360: 'index_360_Q8_2mbps'
 }
